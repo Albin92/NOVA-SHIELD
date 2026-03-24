aca778d2c7943828e4f8446026cf01132d7d11bb
Albin92 <coder9269@gmail.com>
2026-03-24 11:25:32 +0530

    fixed timetable.js

--- timetable-backend/routes/timetable.js
+++ timetable-backend/routes/timetable.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { generate, getAll } = require('../controllers/timetableController');
+const { generate, getAll, clearTimetable, resetAll } = require('../controllers/timetableController');
 router.post('/generate', generate);
 router.get('/',          getAll);
+router.delete('/clear',clearTimetable);
+router.delete('/reset',resetAll);
 module.exports = router;
